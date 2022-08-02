b3b0fe18d957eab9174df7921c3bd955875db9aa
universe-ys <smiletmf@naver.com>
2022-08-02 12:23:43 +0900

    todo list 완성

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,103 @@
 import '@fortawesome/fontawesome-free/js/all.min.js';
 import '../scss/style.scss';
 
+class Router {
+  routes = [];
+  notFoundCallback = () => {};
+  addRoute(url, callback) {
+    this.routes.push({
+      url,
+      callback,
+    });
+    return this;
+  }
+
+  checkRoute() {
+    const currentRoute = this.routes.find(
+      (route) => route.url === window.location.hash,
+    );
+
+    if (!currentRoute) {
+      this.notFoundCallback();
+      return;
+    }
+
+    currentRoute.callback();
+  }
+
+  init() {
+    window.addEventListener('hashchange', this.checkRoute.bind(this));
+
+    if (!window.location.hash) {
+      window.location.hash = '#/';
+    }
+
+    this.checkRoute();
+  }
+
+  setNotFound(callback) {
+    this.notFoundCallback = callback;
+    return this;
+  }
+}
+
+class Storage {
+  saveTodo(id, todoContent) {
+    const todosData = this.getTodos();
+    todosData.push({ id, content: todoContent, status: 'TODO' });
+    localStorage.setItem('todos', JSON.stringify(todosData));
+  }
+
+  editTodo(id, todoContent, status = 'TODO') {
+    const todosData = this.getTodos();
+    const todoIndex = todosData.findIndex((todo) => todo.id == id);
+    const targetTodoData = todosData[todoIndex];
+    const editedTodoData =
+      todoContent === ''
+        ? { ...targetTodoData, status }
+        : { ...targetTodoData, content: todoContent };
+    todosData.splice(todoIndex, 1, editedTodoData);
+    localStorage.setItem('todos', JSON.stringify(todosData));
+  }
+
+  deleteTodo(id) {
+    const todosData = this.getTodos();
+    todosData.splice(
+      todosData.findIndex((todo) => todo.id == id),
+      1,
+    );
+    localStorage.setItem('todos', JSON.stringify(todosData));
+  }
+
+  getTodos() {
+    return localStorage.getItem('todos') === null
+      ? []
+      : JSON.parse(localStorage.getItem('todos'));
+  }
+}
+
 class TodoList {
-  constructor() {
+  storage;
+  inputContainerEl;
+  inputAreaEl;
+  todoInputEl;
+  addBtnEl;
+  todoContainerEl;
+  todoListEl;
+  radioAreaEl;
+  filterRadioBtnEls;
+
+  constructor(storage) {
+    this.initStorage(storage);
     this.assignElement();
     this.addEvent();
+    this.loadSavedData();
   }
+
+  initStorage(storage) {
+    this.storage = storage;
+  }
+
   assignElement() {
     this.inputContainerEl = document.getElementById('input-container');
     this.inputAreaEl = this.inputContainerEl.querySelector('#input-area');
@@ -18,10 +110,19 @@ class TodoList {
       'input[name="filter"]',
     );
   }
+
   addEvent() {
     this.addBtnEl.addEventListener('click', this.onClickAddBtn.bind(this));
     this.todoListEl.addEventListener('click', this.onClickTodoList.bind(this));
     this.addRadioBtnEvent();
+  }
+
+  loadSavedData() {
+    const todosData = this.storage.getTodos();
+    for (const todoData of todosData) {
+      const { id, content, status } = todoData;
+      this.createTodoElement(id, content, status);
+    }
   }
 
   addRadioBtnEvent() {
@@ -35,7 +136,7 @@ class TodoList {
 
   onClickRadioBtn(event) {
     const { value } = event.target;
-    this.filterTodo(value);
+    window.location.href = `#/${value.toLowerCase()}`;
   }
 
   filterTodo(status) {
@@ -77,6 +178,12 @@ class TodoList {
   completeTodo(target) {
     const todoDiv = target.closest('.todo');
     todoDiv.classList.toggle('done');
+    const { id } = todoDiv.dataset;
+    this.storage.editTodo(
+      id,
+      '',
+      todoDiv.classList.contains('done') ? 'DONE' : 'TODO',
+    );
   }
 
   saveTodo(target) {
@@ -84,6 +191,8 @@ class TodoList {
     todoDiv.classList.remove('edit');
     const todoInputEl = todoDiv.querySelector('input');
     todoInputEl.readOnly = true;
+    const { id } = todoDiv.dataset;
+    this.storage.editTodo(id, todoInputEl.value);
   }
 
   edtitTodo(target) {
@@ -100,6 +209,7 @@ class TodoList {
       todoDiv.remove();
     });
     todoDiv.classList.add('delete');
+    this.storage.deleteTodo(todoDiv.dataset.id);
   }
 
   onClickAddBtn() {
@@ -107,13 +217,19 @@ class TodoList {
       alert('내용을 입력해주세요.');
       return;
     }
-    this.createTodoElement(this.todoInputEl.value);
+    const id = Date.now();
+    this.storage.saveTodo(id, this.todoInputEl.value);
+    this.createTodoElement(id, this.todoInputEl.value);
   }
 
-  createTodoElement(value) {
+  createTodoElement(id, value, status = null) {
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
+    if (status === 'DONE') {
+      todoDiv.classList.add('done');
+    }
 
+    todoDiv.dataset.id = id;
     const todoContent = document.createElement('input');
     todoContent.value = value;
     todoContent.readOnly = true;
@@ -151,5 +267,18 @@ class TodoList {
 
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line no-unused-vars
-  const todoList = new TodoList();
+  const router = new Router();
+  const todoList = new TodoList(new Storage());
+  const routeCallback = (status) => () => {
+    todoList.filterTodo(status);
+    document.querySelector(
+      `input[type="radio"][value="${status}"]`,
+    ).checked = true;
+  };
+  router
+    .addRoute('#/all', routeCallback('ALL'))
+    .addRoute('#/todo', routeCallback('TODO'))
+    .addRoute('#/done', routeCallback('DONE'))
+    .setNotFound(routeCallback('ALL'))
+    .init();
 });
